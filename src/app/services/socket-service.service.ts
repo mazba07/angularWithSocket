405d1bebec6bd92d129016eb60e7922a7578388e
Mazba Uddin <mazba09@gmail.com>
2022-12-23 15:29:07 +0600

    socket is working fine in ssr mood

--- src/app/services/socket-service.service.ts
+++ src/app/services/socket-service.service.ts
@@ -1,25 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { io } from 'socket.io-client';
 import { Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketServiceService {
 
+  isBrowser: boolean | undefined;
   socket: any;
   // uri: string = "ws://localhost:3000";
   uri: string = "http://localhost:3000";
 
-  constructor() {
-    this.socket = io(this.uri);
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+    if (this.isBrowser) {
+      this.socket = io(this.uri);
+    }
   }
 
   listen(eventName: string) {
     return new Observable((subscriber) => {
       this.socket.on(eventName, (data: any) => {
         subscriber.next(data);
-      })
+      });
     });
   }
 
@@ -28,9 +33,7 @@ export class SocketServiceService {
   }
 
   close() {
-    setTimeout(() => {
-      this.socket.close();
-    }, 2000);
+    this.socket.close();
   }
 
 }
